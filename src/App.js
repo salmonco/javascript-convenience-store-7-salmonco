8377efd536556c192766d9aa3fe73499a2581228
salmonco <xkqjsslsek80@naver.com>
2024-11-09 23:15:00 +0900

    feat(app): 상품 목록과 행사 목록을 파일 입출력을 통해 불러오기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 class App {
-  async run() {}
+  async run() {
+    // 상품 목록과 행사 목록을 파일 입출력을 통해 불러온다.
+    const products = [];
+    const filename = fileURLToPath(import.meta.url);
+    const dirname = path.dirname(filename);
+    const filePath = path.join(dirname, '..', 'public', 'products.md');
+    try {
+      const data = await fs.readFile(filePath, 'utf8');
+      data
+        .split('\n')
+        .slice(1, -1)
+        .forEach((line) => {
+          const [name, price, quantity, promotion] = line.split(',');
+          products.push({ name, price, quantity, promotion });
+        });
+    } catch (error) {
+      this.throwError(`파일을 불러오는 중 오류가 발생했습니다. ${error.message}`);
+    }
+  }
+
+  throwError(message) {
+    throw new Error(`[ERROR] ${message}`);
+  }
 }
 
 export default App;
