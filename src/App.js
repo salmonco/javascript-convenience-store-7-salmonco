0208b7c20280d247b527f46fbac4f91c0182271d
salmonco <xkqjsslsek80@naver.com>
2024-11-09 23:20:05 +0900

    feat(app): 상품 목록과 행사 목록을 파일 입출력을 통해 불러오기

--- src/App.js
+++ src/App.js
@@ -19,8 +19,25 @@ class App {
           products.push({ name, price, quantity, promotion });
         });
     } catch (error) {
-      this.throwError(`파일을 불러오는 중 오류가 발생했습니다. ${error.message}`);
+      this.throwError(`products.md 파일을 불러오는 중 오류가 발생했습니다. ${error.message}`);
     }
+
+    const promotions = [];
+    const promotionFilePath = path.join(dirname, '..', 'public', 'promotions.md');
+    try {
+      const data = await fs.readFile(promotionFilePath, 'utf8');
+      data
+        .split('\n')
+        .slice(1, -1)
+        .forEach((line) => {
+          const [name, buy, get, startDate, endDate] = line.split(',');
+          promotions.push({ name, buy, get, startDate, endDate });
+        });
+    } catch (error) {
+      this.throwError(`promotions.md 파일을 불러오는 중 오류가 발생했습니다. ${error.message}`);
+    }
+
+    // 환영 인사와 함께 상품명, 가격, 프로모션 이름, 재고를 안내한다. 만약 재고가 0개라면 재고 없음을 출력한다.
   }
 
   throwError(message) {
