754f5ddd3d430122ec92d50ae7bdb699c0f1c23c
salmonco <xkqjsslsek80@naver.com>
2024-11-11 02:02:57 +0900

    refactor(OutputView): 개행 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -5,11 +5,13 @@ const OutputView = {
   printWelcome() {
     Console.print('안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n');
   },
+
   printProducts(products) {
     products.forEach((product) => {
       Console.print(product.toString());
     });
   },
+
   printReceipt({ promotionBuyProducts, generalBuyProducts, products, bonusProducts, isMembership }) {
     const { totalBuyProducts, ...footerData } = ReceiptCalculator.calculate({
       generalBuyProducts,
@@ -23,13 +25,16 @@ const OutputView = {
     this.printBody(totalBuyProducts, products, bonusProducts);
     this.printFooter(footerData);
   },
+
   printHeader() {
     Console.print('\n==============W 편의점================\n');
   },
+
   printBody(totalBuyProducts, products, bonusProducts) {
     this.printPerProduct(totalBuyProducts, products);
     this.printBonusProducts(bonusProducts);
   },
+
   printPerProduct(totalBuyProducts, products) {
     Console.print('상품명		수량	금액\n');
     Object.entries(totalBuyProducts).forEach(([name, quantity]) => {
@@ -38,12 +43,14 @@ const OutputView = {
       Console.print(`${name}		${quantity}	${totalPricePerProduct.toLocaleString()}\n`);
     });
   },
+
   printBonusProducts(bonusProducts) {
     Console.print('=============증	정===============\n');
     Object.entries(bonusProducts).forEach(([name, quantity]) => {
       Console.print(`${name}		${quantity}\n`);
     });
   },
+
   printFooter({ totalBuyProductQuantity, totalBuyPrice, promotionSalePrice, membershipSalePrice, totalPayPrice }) {
     Console.print('====================================\n');
     Console.print(`총구매액		${totalBuyProductQuantity}	${totalBuyPrice.toLocaleString()}\n`);
