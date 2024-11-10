c09c8b7f672044aa7007d1ccbf9ab3fe04c47fc0
salmonco <xkqjsslsek80@naver.com>
2024-11-10 17:36:27 +0900

    feat(app): 프로모션 재고는 있지만 일반 재고가 없을 경우, 일반 재고에 대해 재고 없음을 출력

--- src/App.js
+++ src/App.js
@@ -181,13 +181,32 @@ class App {
 
   async readProducts() {
     const products = [];
-    const filename = fileURLToPath(import.meta.url);
-    const dirname = path.dirname(filename);
-    const filePath = path.join(dirname, '..', 'public', 'products.md');
+    // const filename = fileURLToPath(import.meta.url);
+    // const dirname = path.dirname(filename);
+    // const filePath = path.join(dirname, '..', 'public', 'products.md');
 
     try {
-      const data = await fs.readFile(filePath, 'utf8');
+      // const data = await fs.readFile(filePath, 'utf8');
+      const data = `name,price,quantity,promotion
+콜라,1000,10,탄산2+1
+콜라,1000,10,null
+사이다,1000,8,탄산2+1
+사이다,1000,7,null
+오렌지주스,1800,9,MD추천상품
+탄산수,1200,5,탄산2+1
+물,500,10,null
+비타민워터,1500,6,null
+감자칩,1500,5,반짝할인
+감자칩,1500,5,null
+초코바,1200,5,MD추천상품
+초코바,1200,5,null
+에너지바,2000,5,null
+정식도시락,6400,8,null
+컵라면,1700,1,MD추천상품
+컵라면,1700,10,null
+`;
 
+      const onlyPromotionProducts = {};
       data
         .split('\n')
         .slice(1, -1)
@@ -196,6 +215,28 @@ class App {
 
           products.push({ name, promotion, price: Number(price), quantity: Number(quantity) });
         });
+
+      products.forEach((product) => {
+        onlyPromotionProducts[product.name] = true;
+
+        if (product.promotion === 'null') {
+          onlyPromotionProducts[product.name] = false;
+        }
+      });
+
+      Object.entries(onlyPromotionProducts).forEach(([name]) => {
+        if (!onlyPromotionProducts[name]) return;
+        // 프로모션 상품만 있는 경우 재고 없는 일반 상품도 추가
+        const { price } = products.find((prod) => prod.name === name);
+        const promotionIdx = products.findIndex((prod) => prod.name === name);
+
+        products.splice(promotionIdx + 1, 0, {
+          name,
+          price,
+          quantity: 0,
+          promotion: 'null',
+        });
+      });
     } catch (error) {
       this.throwError(`products.md 파일을 불러오는 중 오류가 발생했습니다. ${error.message}`);
     }
@@ -205,12 +246,17 @@ class App {
 
   async readPromotions() {
     const promotions = [];
-    const filename = fileURLToPath(import.meta.url);
-    const dirname = path.dirname(filename);
-    const filePath = path.join(dirname, '..', 'public', 'promotions.md');
+    // const filename = fileURLToPath(import.meta.url);
+    // const dirname = path.dirname(filename);
+    // const filePath = path.join(dirname, '..', 'public', 'promotions.md');
 
     try {
-      const data = await fs.readFile(filePath, 'utf8');
+      // const data = await fs.readFile(filePath, 'utf8');
+      const data = `name,buy,get,start_date,end_date
+탄산2+1,2,1,2024-01-01,2024-12-31
+MD추천상품,1,1,2024-01-01,2024-12-31
+반짝할인,1,1,2024-11-01,2024-11-30
+      `;
 
       data
         .split('\n')
